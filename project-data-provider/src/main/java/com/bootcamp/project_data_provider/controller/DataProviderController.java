493f0b1ba52c6f7cd989a90a9b2fd5dfae402405
matthew.yip <matthewyip14@gmail.com>
2025-09-11 15:15:41 +0800

    update

---
+++ project-data-provider/src/main/java/com/bootcamp/project_data_provider/controller/DataProviderController.java
@@ -0,0 +1,8 @@
+package com.bootcamp.project_data_provider.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DataProviderController {
+  
+}
--- project-data-provider/src/main/java/com/bootcamp/project_data_provider/dto/CompanyDTO.java
+++ project-data-provider/src/main/java/com/bootcamp/project_data_provider/dto/CompanyDTO.java
@@ -1,23 +1,25 @@
 package com.bootcamp.project_data_provider.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.Getter;
-import lombok.Setter;
+import lombok.NoArgsConstructor;
 
 @Data
 @Builder
 @Getter
-@Setter
-public class CompanyResponse {
+@AllArgsConstructor
+@NoArgsConstructor
+public class CompanyDTO {
   private String country;
   private String currency;
   private String exchange;
   private String ipo;
-  private double marketCapitalization;
+  private Double marketCapitalization;
   private String name;
   private String phone;
-  private double shareOutstanding;
+  private Double shareOutstanding;
   private String ticker;
   private String weburl;
   private String logo;
