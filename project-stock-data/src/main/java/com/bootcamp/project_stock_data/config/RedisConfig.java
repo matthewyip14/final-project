ffffdbb51f40478715b71e53ced264764980149a
matthew.yip <matthewyip14@gmail.com>
2025-09-12 02:30:54 +0800

    update

--- project-stock-data/src/main/java/com/bootcamp/project_stock_data/config/RedisConfig.java
+++ project-stock-data/src/main/java/com/bootcamp/project_stock_data/config/RedisConfig.java
@@ -3,6 +3,7 @@ package com.bootcamp.project_stock_data.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
 import org.springframework.web.client.RestTemplate;
@@ -11,6 +12,12 @@ import com.bootcamp.project_stock_data.model.dto.CompanyDTO;
 
 @Configuration
 public class RedisConfig {
+
+  @Bean
+  public RedisConnectionFactory redisConnectionFactory() {
+    return new LettuceConnectionFactory("localhost", 6379); // 確保與實際 Redis 地址匹配
+  }
+
   @Bean
   public RedisTemplate<String, CompanyDTO> redisTemplate(RedisConnectionFactory factory) {
     RedisTemplate<String, CompanyDTO> template = new RedisTemplate<>();
