83967b9ecd0145b13aae242cef2e97f8f3026f66
matthew.yip <matthewyip14@gmail.com>
2025-09-16 01:35:18 +0800

    update

--- project-stock-data/src/main/java/com/bootcamp/project_stock_data/entity/StockEntity.java
+++ project-stock-data/src/main/java/com/bootcamp/project_stock_data/entity/StockEntity.java
@@ -7,10 +7,16 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import lombok.Data;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Entity
 @Table(name = "stocks")
 @Data
+@NoArgsConstructor
+@Getter
+@Setter
 public class StockEntity {
   @Id
   @GeneratedValue(strategy = GenerationType.IDENTITY)
