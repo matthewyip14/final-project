713679fcfc18120effda22cdc6fe975de7401b54
matthew.yip <matthewyip14@gmail.com>
2025-09-16 01:09:03 +0800

    update

---
+++ project-data-provider/src/main/java/com/bootcamp/project_data_provider/config/AppConfig.java
@@ -0,0 +1,15 @@
+package com.bootcamp.project_data_provider.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
+
+@Configuration
+public class AppConfig {
+
+	@Bean
+	public RestTemplate restTemplate() {
+		return new RestTemplate();
+	}
+
+}
