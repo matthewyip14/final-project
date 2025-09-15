11e630a08237c785bd3f92570d5d0035998e7134
matthew.yip <matthewyip14@gmail.com>
2025-09-16 00:53:56 +0800

    update

--- project-stock-data/src/test/java/com/bootcamp/project_stock_data/ProjectStockDataApplicationTests.java
+++ project-stock-data/src/test/java/com/bootcamp/project_stock_data/ProjectStockDataApplicationTests.java
@@ -3,7 +3,7 @@ package com.bootcamp.project_stock_data;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(properties = {"spring.profiles.active=test"})
 class ProjectStockDataApplicationTests {
 
 	@Test
