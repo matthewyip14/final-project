99e115b27b499e81d4376d4b5a0bcad5fd3317f4
matthew.yip <matthewyip14@gmail.com>
2025-09-19 00:16:52 +0800

    update

--- project-heatmap-ui/src/main/java/com/bootcamp/project_heatmap_ui/controller/ViewController.java
+++ project-heatmap-ui/src/main/java/com/bootcamp/project_heatmap_ui/controller/ViewController.java
@@ -2,11 +2,17 @@ package com.bootcamp.project_heatmap_ui.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @Controller
 public class ViewController {
   @GetMapping("/heatmap")
   public String heatmap() {
       return "index";
+  }
+
+  @GetMapping("/ohlc-view/{symbol}")
+  public String ohlc(@PathVariable String symbol) {
+      return "ohlc";
   }
 }
--- project-heatmap-ui/src/main/resources/static/js/candlestick.js
+++ project-heatmap-ui/src/main/resources/static/js/candlestick.js
@@ -1,12 +1,4 @@
 function showCandlestick(symbol) {
-    fetch(`/data/ohlc/${symbol}`)
-        .then(response => response.json())
-        .then(data => {
-            d3.select("#candlestick").style("display", "block").html("");
-            const width = 800, height = 400;
-            const svg = d3.select("#candlestick").append("svg").attr("width", width).attr("height", height);
-            // 簡化 candlestick 邏輯，使用 D3 繪製 OHLC
-            // 省略詳細實現，假設使用線條和矩形表示蠟燭
-            // ...
-        });
+    const url = `/ohlc-view/${symbol}`;
+    window.open(url, '_blank');
 }
